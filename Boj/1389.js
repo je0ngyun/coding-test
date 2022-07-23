aecd2603dc79479bdee5f166fe2e8a5ecb780bd6
김정윤 <kjyjh9930@gmail.com>
2022-07-23 17:13:04 +0900

    solve : Boj 케빈 베이컨의 6단계 법칙

---
+++ Boj/1389.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const input = require('fs')
+  .readFileSync('./Boj/input/1389.stdin')
+  .toString()
+  .trim()
+  .split('\n')
+  .map((inn) => inn.split(' ').map((ins) => Number(ins)));
+
+const PN = input[0][0];
+
+const result = Array(PN + 1).fill(0);
+
+const graph = Array(input[0][0] + 1)
+  .fill(0)
+  .map(() => []);
+
+input.slice(1).forEach(([from, to]) => {
+  graph[from].push(to);
+  graph[to].push(from);
+});
+
+const bfs = (start) => {
+  const visited = new Array(PN + 1).fill(false);
+  const queue = [[start, 0]];
+  visited[start] = true;
+  while (queue.length) {
+    const [node, weight] = queue.shift();
+    result[start] += weight;
+    for (const linked of graph[node]) {
+      if (!visited[linked]) {
+        visited[linked] = true;
+        queue.push([linked, weight + 1]);
+      }
+    }
+  }
+};
+
+for (let i = 1; i < PN + 1; i++) {
+  bfs(i);
+}
+
+result.shift();
+
+console.log(result.indexOf(Math.min(...result)) + 1);
