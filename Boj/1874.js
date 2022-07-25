0fa8344a8c14e1144d27e9746181bf7365de8a9e
김정윤 <kjyjh9930@gmail.com>
2022-07-25 22:18:04 +0900

    solve : 1874 스택수열

--- Boj/1874.js
+++ Boj/1874.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict';
 /**
  * Boj 1874
  */
 const qn = 1874;
 const filePath =
-  process.platform === "linux" ? "./dev/stdin" : `./Boj/input/${qn}.stdin`;
+  process.platform === 'linux' ? './dev/stdin' : `./Boj/input/${qn}.stdin`;
 
-const input = require("fs")
+const input = require('fs')
   .readFileSync(filePath)
   .toString()
   .trim()
-  .split("\n");
+  .split('\n');
 
 const target = input
   .slice(1)
@@ -22,19 +22,22 @@ const numArr = new Array(Number(input[0]))
   .map((_, idx) => idx + 1)
   .reverse();
 
-console.log(numArr);
-console.log(target);
+const op = [];
 
-while (numArr.length) {
-  if (!stack.length) {
+while (numArr.length > 0) {
+  if (stack[stack.length - 1] !== target[target.length - 1]) {
     stack.push(numArr.pop());
+    op.push('+');
   }
-  if (target[target.length - 1] !== numArr[numArr.length - 1]) {
-    stack.push(numArr.pop());
-  }
-  if (target[target.length - 1] === numArr[numArr.length - 1]) {
-    stack.pop();
+  if (stack[stack.length - 1] === target[target.length - 1]) {
+    op.push('-');
     target.pop();
+    stack.pop();
   }
-  console.log(stack);
+}
+
+if (target.join('') === stack.join('')) {
+  console.log(op.join('\n') + '\n' + Array(target.length).fill('-').join('\n'));
+} else {
+  console.log('NO');
 }
