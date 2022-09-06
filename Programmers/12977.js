9f39aa23b2248cd3e50b2634e668b8b4587854e9
김정윤 <kjyjh9930@gmail.com>
2022-09-06 21:37:47 +0900

    solve

--- Programmers/12977.js
+++ Programmers/12977.js
@@ -1,5 +1,32 @@
 'use strict';
 
-const solution = (nums) => {
-  return 'ing';
+let result = 0;
+
+const isPrime = (num) => {
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.floor(Math.sqrt(num)); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
 };
+
+const combi = (arr, select, depth = 0, ret = []) => {
+  if (ret.length === select) {
+    console.log(ret);
+    //if (isPrime(ret.reduce((a, b) => a + b))) result += 1;
+    return;
+  }
+  if (depth > arr.length - 1) {
+    return;
+  }
+  combi(arr, select, depth + 1, [...ret]);
+  ret.push(arr[depth]);
+  combi(arr, select, depth + 1, [...ret]);
+};
+
+const solution = (nums) => {
+  combi(nums, 3);
+  return result;
+};
+
+//console.log(solution([1, 2, 7, 6, 4]));
