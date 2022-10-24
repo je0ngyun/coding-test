e073f6c7b445dbb98f599ea9861027596f387b2b
김정윤 <kjyjh9930@gmail.com>
2022-10-24 22:28:54 +0900

    solve : [leetcode] - https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/submissions/

--- Leetcode/1239.js
+++ Leetcode/1239.js
@@ -10,9 +10,10 @@ const maxLength = (arr) => {
     }
     const origin = selected + arr[depth];
     if (origin.length === new Set(origin).size) {
-      dfs(arr, selected + arr[depth], depth + 1);
+      // 현재 기록된 문자열과 다음 문자열을 합칠 수 있는경우
+      dfs(arr, origin, depth + 1);
     }
-    dfs(arr, selected, depth + 1);
+    dfs(arr, selected, depth + 1); // 현재 depth를 건너뛸 경우
   };
   dfs(arr);
 
