dbc435b9057cbc3e9695896720164bc5f7445fab
김정윤 <kjyjh9930@gmail.com>
2022-11-12 13:00:28 +0900

    solve : [leetcode] - https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/841725104/

--- Leetcode/26.js
+++ Leetcode/26.js
@@ -1,9 +1,7 @@
 const removeDuplicates = (nums) => {
   let refIdx = 0;
-  const max = Math.max(...nums);
-  const min = Math.min(...nums);
-  for (let i = min; i <= max; i++) {
-    if (nums.includes(i)) nums[refIdx++] = i;
+  for (let value = nums[0]; value <= nums[nums.length - 1]; value++) {
+    if (nums.includes(value)) nums[refIdx++] = value;
   }
   return refIdx;
 };
