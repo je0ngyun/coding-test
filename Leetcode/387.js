02a151409c2281769240fb5fd682c0621e38ebbf
김정윤 <kjyjh9930@gmail.com>
2022-11-15 23:09:03 +0900

    solve : [leetcode] - https://leetcode.com/problems/first-unique-character-in-a-string/description/

--- Leetcode/387.js
+++ Leetcode/387.js
@@ -6,6 +6,6 @@ const firstUniqChar = (s) =>
       acc[cur] ? acc[cur].cnt++ : (acc[cur] = { cnt: 1, idx });
       return acc;
     }, {}),
-  ).filter(([_, { cnt }]) => cnt === 1)[0]?.[1].idx ?? -1;
+  ).find(([_, { cnt }]) => cnt === 1)?.[1].idx ?? -1;
 
 console.log(firstUniqChar('leetcode'));
